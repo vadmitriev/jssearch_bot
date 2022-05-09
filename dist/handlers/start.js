10468d9f121451d4d7684b4b2da0969aaa984569
dmitrievva <v.a.dmitriev123@gmail.com>
2022-05-09 13:41:06 +0500

    Remove dist folder

--- dist/handlers/start.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const start = (ctx) => {
-    console.log('start');
-    ctx.reply(`Hello ${ctx.from.first_name}!\nFind something about javascript and web development.`);
-};
-exports.default = start;
-//# sourceMappingURL=start.js.map
